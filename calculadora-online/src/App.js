a527b9e19b01af8336f5175e2c00bc7c9a73c476
cecilia reynoso <cecireynoso@gmail.com>
2023-11-25 17:42:10 -0300

    calculator structure

--- calculadora-online/src/App.js
+++ calculadora-online/src/App.js
@@ -1,12 +1,46 @@
 import './App.css';
+import Pascal from './images/Pascal.png';
+import Boton from './components/Boton';
 
-function App() {
-  return (
-    <div className="App">
-      Hola
+  function App() {
+    return (
+      <div className="App">
+        <div className="logo-contenedor">
+          <img className='logo-pascal'
+          src= {Pascal}
+          alt= 'Pascal Portrait' />
+        </div>
+        <div className="contenedor-calculadora">
+          <div className="fila">
+            <Boton>1</Boton>
+            <Boton>2</Boton>
+            <Boton>3</Boton>
+            <Boton>+</Boton>
+          </div>
+          <div className="fila">
+            <Boton>4</Boton>
+            <Boton>5</Boton>
+            <Boton>6</Boton>
+            <Boton>-</Boton>
+          </div>
+          <div className="fila">
+            <Boton>7</Boton>
+            <Boton>8</Boton>
+            <Boton>9</Boton>
+            <Boton>*</Boton>
+          </div>
+          <div className="fila">
+            <Boton>=</Boton>
+            <Boton>0</Boton>
+            <Boton>.</Boton>
+            <Boton>/</Boton>
+            </div>
+          <div className="fila">
 
-    </div>
-  );
-}
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-export default App;
+  export default App;
